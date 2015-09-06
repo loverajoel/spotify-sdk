7f5495b2218a1c5bbd847e8b5f17f8f9c3d9c1e9
Joel <joelalovera@gmail.com>
2015-09-06 03:40:52 -0300

    remove old code

--- src/Factory.js
+++ src/Factory.js
@@ -68,8 +68,5 @@ var Factory = function(data) {
 };
 
 export default function(data) {
-    // return new Promise((resolve) => {
-        // resolve(new Factory(data));
-    // })
     return new Factory(data);
 };
