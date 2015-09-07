397e9b81634df49f98a506dd31e1961ff5419b9a
Joel <joelalovera@gmail.com>
2015-09-06 23:57:06 -0300

    expose all methods

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-import Client from './src/Client';
-import PlaylistHandler from './src/handlers/PlaylistHandler';
-import UserHandler from './src/handlers/UserHandler';
-import TrackHandler from './src/handlers/TrackHandler';
-import UserEntity from './src/entities/UserEntity';
-import PlaylistEntity from './src/entities/PlaylistEntity';
+import Client from './Client';
+import ArtistHandler from './handlers/ArtistHandler';
+import AlbumHandler from './handlers/AlbumHandler';
+import TrackHandler from './handlers/TrackHandler';
+import PlaylistHandler from './handlers/PlaylistHandler';
+import UserHandler from './handlers/UserHandler';
+import CollectionHandler from './handlers/CollectionHandler';
+import ArtistEntity from './entities/ArtistEntity';
+import AlbumEntity from './entities/AlbumEntity';
+import TrackEntity from './entities/TrackEntity';
+import PlaylistEntity from './entities/PlaylistEntity';
+import UserEntity from './entities/UserEntity';
+import CollectionEntity from './entities/CollectionEntity';
+import Artist from './halpers/Artist';
+import Album from './halpers/Album';
+import Track from './halpers/Track';
+import Playlist from './halpers/Playlist';
+import User from './halpers/User';
+import Collection from './halpers/Collection';
 
 export default {
 	Client: Client,
+	ArtistHandler: ArtistHandler,
+	AlbumHandler: AlbumHandler,
+	TrackHandler: TrackHandler,
 	PlaylistHandler: PlaylistHandler,
 	UserHandler: UserHandler,
-	TrackHandler: TrackHandler,
+	CollectionHandler: CollectionHandler,
+	ArtistEntity: ArtistEntity,
+	AlbumEntity: AlbumEntity,
+	TrackEntity: TrackEntity,
+	PlaylistEntity: PlaylistEntity,
 	UserEntity: UserEntity,
-	PlaylistEntity: PlaylistEntity
+	CollectionEntity: CollectionEntity,
+	Artist: Artist,
+	Album: Album,
+	Track: Track,
+	Playlist: Playlist,
+	User: User,
+	Collection: Collection
 };
